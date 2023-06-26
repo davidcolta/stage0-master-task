7953e9b7ea8121ff3b7c0db41258591b96fcf298
David Colta <breaddude2005@gmail.com>
2023-06-26 13:44:20 +0300

    5th

--- src/main/java/com/epam/algorithms/AlgorithmComplexity.java
+++ src/main/java/com/epam/algorithms/AlgorithmComplexity.java
@@ -1,32 +1,42 @@
 package com.epam.algorithms;
 
+import com.epam.algorithms.utils.Complexity;
+
+/**
+ * Tasks for assessing the algorithm complexity. The following are the ratios of the constants with the BigO estimate:
+ * <p>
+ * Complexity.O_LOG_N       -> O(log N) Complexity.O_N_2         -> O(log N^2) Complexity.O_FACTORIAL_N -> O(!N)
+ * Complexity.O_2_N         -> O(2 * N) Complexity.O_N_LOG_N     -> O(N * log N) Complexity.O_1           -> O(1)
+ * Complexity.O_N           -> O(N)
+ */
 public class AlgorithmComplexity {
-    public static Complexity badLinerSearch() {
-        // Linear search complexity if the element is at the end of the array
-        // Complexity: O(N)
+
+    /**
+     * Return the linear search complexity if the element you are looking for is at the end of the array.
+     */
+    public Complexity badLinerSearch() {
         return Complexity.O_N;
     }
 
-    public static Complexity arrayIndexItemAccess() {
-        // Complexity of accessing an array element by index
-        // Complexity: O(1)
+    /**
+     * Return the complexity of accessing an array element by index.
+     */
+    public Complexity arrayIndexItemAccess() {
         return Complexity.O_1;
     }
 
-    public static Complexity binarySorting() {
-        // Complexity of binary search
-        // Complexity: O(log N)
+    /**
+     * Return the complexity of a binary search.
+     */
+    public Complexity binarySorting() {
         return Complexity.O_LOG_N;
     }
 
-    public static Complexity twoCycleSorting() {
-        // Complexity of the Two Cycle Sorting algorithm
-        // Complexity: O(N^2)
+    /**
+     * Return the complexity of the twoCycleSorting algorithm.
+     */
+    public Complexity twoCycleSorting() {
         return Complexity.O_N_2;
     }
 
-    // Constants representing the Big-O estimates
-    public enum Complexity {
-        O_LOG_N, O_N_2, O_FACTORIAL_N, O_2_N, O_N_LOG_N, O_1, O_N
-    }
 }
